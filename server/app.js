9da5bd48c200575cd438d1ceb297d398337456d8
jinjin <jinjinx28@gmail.com>
2026-05-18 14:16:45 +0900

    2026-05-18 commit

--- server/app.js
+++ server/app.js
@@ -2,20 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import productsRouter from './routes/products.js';
-// import returnRouter from './routes/return.js';
-// import qnaRouter from './routes/qna.js'; 
+import returnRouter from './routes/return.js';
 
 dotenv.config();
 
 const PORT = process.env.SERVER_PORT || 9000;
 const app = express();
 
+//미들웨어 -> 공통작업 정의
 app.use(cors());
 app.use(express.json());
 
+//라우팅 작업
 app.use('/products', productsRouter);
-// app.use('/return', returnRouter);
-// app.use('/qna', qnaRouter); 
+app.use('/return', returnRouter);
+
 
 app.listen(PORT, () => {
     console.log(`서버 실행 => ${PORT}`);    
