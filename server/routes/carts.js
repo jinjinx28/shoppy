9c5a415be8ade05d5d5f0b76b9e813ba6706872c
jinjin <jinjinx28@gmail.com>
2026-05-26 13:48:53 +0900

    2026-05-26 commit

--- server/routes/carts.js
+++ server/routes/carts.js
@@ -1,12 +1,19 @@
 import express from 'express';
-import * as controller from '../controller/carts.js';
+import * as controller from "../controller/carts.js";
+import { verifyToken } from "../controller/member.js"; // 토큰 유효 기간 확인 미들웨어 추가
 
 const router = express.Router();
 
-router.post('/add', controller.getAdd);
-router.post('/count', controller.getCount);
-router.post('/list', controller.getList);
-router.put('/qty', controller.getQtyUpdate);
-router.delete('/del', controller.getDelete);
+router.post("/add", verifyToken, controller.getAdd);
+router.post("/count", verifyToken, controller.getCount);
+router.post("/list", verifyToken, controller.getList);
+router.put("/qty", verifyToken, controller.getQtyUpdate);
+router.delete("/del", verifyToken, controller.getDelete);
+
+// router.post('/add', controller.getAdd);
+// router.post('/count', controller.getCount);
+// router.post('/list', controller.getList);
+// router.put('/qty', controller.getQtyUpdate);
+// router.delete('/del', controller.getDelete);
 
 export default router;
--- server/routes/member.js
+++ server/routes/member.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 router.post('/signup', controller.getSignup);
 router.post('/idCheck', controller.getIdCheck);
-router.post('/login', controller.getLogin);
+router.post("/login", controller.getLogin);
+router.post("/logout", controller.getLogout);
+router.post("/refresh", controller.getRefresh);
 
 
 export default router;
